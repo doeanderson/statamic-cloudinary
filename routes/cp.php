863e44cd30f4473f726601bb659eec95418eb2ac
Kenton Glass <notnek@users.noreply.github.com>
2023-05-09 16:03:02 -0400

    [update] dependencies and route registration for laravel 9/10

--- routes/cp.php
+++ routes/cp.php
@@ -1,10 +1,11 @@
 <?php
 
+use DoeAnderson\StatamicCloudinary\Http\Controllers\ConfigController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('cloudinary.')->prefix('cloudinary')->group(function () {
     Route::name('config.')->prefix('config')->group(function () {
-        Route::name('edit')->get('/edit', 'ConfigController@edit');
-        Route::name('update')->post('/edit', 'ConfigController@update');
+        Route::name('edit')->get('/edit', [ConfigController::class, 'edit']);
+        Route::name('update')->post('/edit', [ConfigController::class, 'update']);
     });
 });
